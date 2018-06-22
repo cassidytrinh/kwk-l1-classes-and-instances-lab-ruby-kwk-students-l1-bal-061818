5c42eff15fbf14a968b0dfc2ddf8b9cc8db4fdee
cassidytrinh <cassidytrinh@gmail.com>
2018-06-22 18:36:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,12 @@
 # dog.rb
 class Dog
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end 
 
-  def initialize
+fido = Dog.new("Fido")
+snoopy = Dog.new("Snoopy")
+lassie = Dog.new("Lassie")
     
